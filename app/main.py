d7c9ae76563a655a25de85cd2e57c74bddf6c5c4
trejdof <dopster1v9@gmail.com>
2025-06-11 22:07:22 +0200

    main: Tests backup

--- app/main.py
+++ app/main.py
@@ -2,15 +2,19 @@ from app.models.interval import Interval
 from app.models.configuration import Configuration
 from app.models.time import Time
 from app.repositories.config_repo import *
+from app.repositories.state_repo import *
 from app.constants import DAYS_OF_WEEK
 
 def main():
 
-    delete_config("test1")
-    delete_config("test2")
-    delete_config("test4")
-    print(json.dumps(load_all_configs(), indent=4))
+    state = load_state()
+    print_state(state)
 
+    print("##########################")
+    current_time = Time(19, 31)
+    change_selected_configuration('first',current_time)
+    state = load_state()
+    print_state(state)
 
 
 if __name__ == "__main__":
