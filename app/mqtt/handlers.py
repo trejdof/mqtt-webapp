deb96e0a7052d89b216f03d4d77895b4c4dc8cb0
trejdof <dopster1v9@gmail.com>
2025-06-15 23:01:43 +0200

    dump(handlers): Just a backup commit

--- app/mqtt/handlers.py
+++ app/mqtt/handlers.py
@@ -1,7 +1,18 @@
 from app.repositories.state_repo import temp_heartbeat
 
 def handle_temperature_ping(temp):
-    temp_heartbeat(temp)
+    toggle_boiler = temp_heartbeat(temp)
+
+    if toggle_boiler:
+        toggle_boiler()
+
 
 def handle_boiler_ack(payload):
+    return 0
+
+
+# TODO  Publish MQTT message to the boiler TOPIC
+#       Wait for ACK by the MCU on MQTT
+#       Change boiler state in STATE
+def toggle_boiler():
     return 0
